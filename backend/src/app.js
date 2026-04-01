ea31d3ffc565f52a1a23d7acd6420c60cf121e8b
Levan Batmanishvili <levanbatmanishvili@gmail.com>
2026-04-01 23:17:03 +0200

    feat: create user model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import sequelize from "./config/database.js";
+import "./models/user.model.js";
 
 
 const app = express();
@@ -16,6 +17,11 @@ sequelize
   .authenticate()
   .then(() => console.log("Database connected"))
   .catch((err) => console.error("Database error:", err));
+
+  sequelize
+  .sync()
+  .then(() => console.log("Database synchronized"))
+  .catch((error) => console.error("Sync error:", error));
 
 // Test route
 app.get("/api", (req, res) => {
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database.js";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    role: {
+      type: DataTypes.ENUM("client", "expert"),
+      allowNull: false,
+      defaultValue: "client",
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: true,
+  }
+);
+
+export default User;
